e5cd12b6862de6a270db034b0514307b3f3cdd75
johan-martinez <johan.martinez01@uptc.edu.co>
2021-02-28 18:30:50 -0500

    test 1

--- middleware/index.js
+++ middleware/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 var app = express()
 var port = 3000
 
-app.use(express.json())
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
