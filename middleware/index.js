7decdd1f1031e65803806a39830ad05294e33738
johan-martinez <johan.martinez01@uptc.edu.co>
2021-02-28 19:01:39 -0500

    test 4

--- middleware/index.js
+++ middleware/index.js
@@ -7,8 +7,8 @@ const bodyParser = require('body-parser');
 var app = express()
 var port = 3000
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(cors())
 
